9bf22e47b64107dec068c9427e90202360b62575
dennernemethz <134553090+dennernemethz@users.noreply.github.com>
2024-02-09 15:38:53 -0300

    Initial commit

---
+++ javascriptconsole/ex.015.js
@@ -0,0 +1,8 @@
+var velocidade = 14
+console.log(`a velocidade é ${velocidade}`)
+if(velocidade > 90) { // uma condição simples
+    console.log('voce esta dirigindo a cima da velocidade')
+}
+console.log('voce esta dirigindo na velocidade perimitida')
+
+// usando condição simples com IF 
---
+++ javascriptconsole/ex.016.js
@@ -0,0 +1,12 @@
+// usando condição composta e simples
+// na condição composta onde surge o else
+// o else nao precia de função "() " igual o if
+
+var letra1 = "A"
+var letra2 = 'C'
+
+if(letra1==='A'){
+    console.log('essa letra é vogal')
+}else{
+    console.log('é consoante')
+}
