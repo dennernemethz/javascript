11232a7b154c9ec6f6000e47b2071f217f1add24
dennernemethz <134553090+dennernemethz@users.noreply.github.com>
2024-07-03 11:48:37 -0300

    at

---
+++ javascriptconsole/ex.017.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`estou vivendo no ${pais}`)
+if(pais == 'brasil'){
+    console.log('voce é brasileiro')
+}else{
+    console.log('voce é estrangeiro')
+}
---
+++ para praticar/treinando02.js
@@ -0,0 +1,5 @@
+var vel = 90
+if(vel > 40){
+    console.log('voce ultrapassou a velocidade permitida')
+}
+console.log('dirija sempre com cinto de segurança')
