9daca41019db0d314df73700f387242357037e7f
dennernemethz <134553090+dennernemethz@users.noreply.github.com>
2024-07-12 15:48:25 -0300

    att

--- contador/script.js
+++ contador/script.js
@@ -1,27 +1,22 @@
-function verificar(){
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if(fano.value.length == 0 || Number(fano.value) > ano){
-        window.alert('erro')
-    }else{
-        var fsex = window.document.getElementsByName('redsex')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        var img = document.createElement ('img')
-        img.setAttribute('id','foto')
-        if(fsex[0].checked){
-            genero = 'homem'
-            if(idade <=10 ){
-                img.setAttribute('src', 'tedt-yy.jpeg')
-            }
-       
-        }else if (fsex[1].checked) {
-            genero = 'feminino'
+function clicar(){
+    let ini = window.document.getElementById('txt1')
+    let fim = window.document.getElementById('txt2')
+    let passo = window.document.getElementById('txt3')
+    let res = window.document.getElementById('res')
 
-        }
-    } res.innerHTML = ` detectamos ${genero} de ${idade}anos`
-      res.style.textAlign = center
-      res.appendChild(img)
+
+if(ini.value == 0 || fim.value == 0 || passo.value == 0){
+    window.alert('[ERRO]')
+}else{
+    let i =  Number(ini.value)
+    let f =  Number(fim.value)
+    let p =  Number(passo.value)
+    let c = i
+    while(c <= f){
+        c += p
+        res.innerHTML += `${c}`
+    }
+    
+}
+
 }
