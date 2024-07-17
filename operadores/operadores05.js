05d1df21a6847be505db9f5417896a29dbb7c811
dennernemethz <134553090+dennernemethz@users.noreply.github.com>
2024-07-16 21:07:20 -0300

    att

---
+++ operadores/operadores05.js
@@ -0,0 +1,20 @@
+// operadores de incremento, inversão e concatenação
+
+
+/*let num = 10 
+let num2 = -num
+console.log(num2) // nessa operação de positivo passa a ser negativo*/
+
+/*let num = -10 
+let num2 = -num 
+console.log(num2)*/  // aqui ja passa a ser positivo pois - com - = + 
+
+//console.log(num ++ ) // pre incremento, ele usa primeiro o valor de num que no caso e 10, e depois incremeta com +1 
+
+// console.log(++num) // pós incremento pois ele ja usou o operador  para incrementar o valor atribuido no num que agora é 11
+
+/*let num = 10 
+let num2 = 11 
+
+console.log( num + " " + num2)*/ // concatenção valores atribuidos como string, vai surgir a concatenação e nao a soma.
+
