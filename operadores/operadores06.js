0f2457904dc62f724a3423353c57137bf57d64ba
dennernemethz <134553090+dennernemethz@users.noreply.github.com>
2024-07-17 09:10:06 -0300

    att

---
+++ operadores/operadores06.js
@@ -0,0 +1,8 @@
+// operadores ternarios para saber se a consição logica e verdadeira ou falsa
+
+let n1 = 10 
+let n2 = 20 
+
+let soma =  ((n1 + n2) >= 30 ? "verdadeiro": "falso")
+
+console.log(soma)
