784f0e03a361b6ccc9002998e96a20f5112f4d24
dennernemethz <134553090+dennernemethz@users.noreply.github.com>
2024-07-22 11:23:07 -0300

    att

---
+++ repetiçoes-html/repetiçoes-js02.js
@@ -0,0 +1,21 @@
+// repetiçoes com for in e for of 
+
+
+let num = [10,3,2,4,2,17,8] 
+
+/*for(o in num){     // vai mostrar somente a posição dos elementos dentro de um array
+    console.log(o)
+}
+
+console.log(' fim do programa ')*/
+
+/*for(o of num){  // esse aqui vai mostrar os elementos dentro de um array 
+ console.log(o)
+}
+
+console.log(' fim do programa')*/
+
+
+for(let n = 0; n<num.length; n++){
+    console.log(num[n])
+}
---
+++ repetiçoes-html/repetiçoes-js03.js
@@ -0,0 +1,9 @@
+// uso do while  
+
+
+let n1 = 1
+
+while(n1<10){  // enquanto o n1 for menor que 10, vai se repitir encrementando com ++1 ate o n1 se tornar 10 
+    console.log(n1)
+    n1++
+}
