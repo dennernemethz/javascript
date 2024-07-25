f0793ac0d186858cfe8d2f82d791d96a8d8a6b55
dennernemethz <134553090+dennernemethz@users.noreply.github.com>
2024-07-25 14:49:27 -0300

    att

---
+++ javascriptconsole/functions/function06.js
@@ -0,0 +1,6 @@
+// usando function com comparação v e f 
+
+
+function calc (n){
+    return ((n<20) && (n>30))
+} console.log(calc(22))
--- t.js
+++ t.js
@@ -1,4 +1,4 @@
-// usando break 
-
-
-
+function calc (n){
+    return ((n>22) && (n<10)) 
+    
+} console.log(calc(15))
