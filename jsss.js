889f9347c68b286f7004c176be5ede12d2dc5672
dennernemethz <134553090+dennernemethz@users.noreply.github.com>
2024-08-06 09:01:45 -0300

    add

--- jsss.js
+++ jsss.js
@@ -1,22 +1,7 @@
-let dc1 = window.document.getElementById('c1')
-let dc2 = window.document.getElementById('c2')
-let dc3 = window.document.getElementById('c3')
-let dc4 = window.document.getElementById('c4')
-let dc5 = window.document.getElementById('c5')
-let dc6 = window.document.getElementById('c6')
+function soma(n1){
+  let n2 = 10 
+  let soma = n1 + n2 
+  return soma 
+}
 
-const arraysElemetos=[dc1,dc2,dc3,dc4,dc5,dc6]
-
-/*console.log(arraysElemetos)
-
-for(n of arraysElemetos)
-    n.innerHTML='cfb'*/ 
-
-
-arraysElemetos.map((e)=>{
-   window.document.write = e
-   e.innerHTML='cb'
-})
-
-
-
+console.log(soma(2))
