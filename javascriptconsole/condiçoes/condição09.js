95fdce869e3254221cc1da91907686b654eb031a
dennernemethz <134553090+dennernemethz@users.noreply.github.com>
2024-08-10 10:16:37 -0300

    att

---
+++ javascriptconsole/condiçoes/condição09.js
@@ -0,0 +1,9 @@
+
+
+const numero =[1,33,15,21,34,27,55,12]
+const maior=numero.filter(function (val) { 
+    if(val =>18)
+        return val
+ })
+
+ console.log(maior)
--- jsss.js
+++
@@ -1,7 +0,0 @@
-function soma(n1){
-  let n2 = 10 
-  let soma = n1 + n2 
-  return soma 
-}
-
-console.log(soma(2))
