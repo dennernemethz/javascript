bfba8d96b192247be822410ed58a9ff8577904d9
denner nemeth <134553090+dennernemethz@users.noreply.github.com>
2024-08-19 10:26:27 -0300

    att

---
+++ indexTest.js
@@ -0,0 +1,4 @@
+ let num = [10, 11, 13, 14, 15,['numeros','string','boolean']]
+ let num01= [16,17,18, num]
+
+console.log(num01[3][5])
---
+++ javascriptconsole/arrays-vetores/array12.js
@@ -0,0 +1,6 @@
+//adicionando elementos com o push
+
+let num = [1,2,3]
+console.log(num)
+console.log(num.push('4'))//adicionou o 4 no fim do array
+console.log(num.length)
