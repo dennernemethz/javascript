2110e25ebc2d24e9d5b08a80494d4dc49fd2be6f
denner nemeth <134553090+dennernemethz@users.noreply.github.com>
2024-08-28 20:50:38 -0300

    att

--- indexTest.js
+++ indexTest.js
@@ -1,16 +1,5 @@
-class int {
-    constructor(nn){
-    this.n=nn
-    }
+function rnd(n) {
+return Math.floor(Math.random()*n)
 }
 
-let n1 = new int(30)
-let n2 = new int(20)
-
-let res = n1.n + n2.n
-
-if(res <= 40){
-    console.log('menor')
-}else{
-    console.log('maior')
-}
+console.log(rnd(50))
