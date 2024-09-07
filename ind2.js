901d99c0f682ddfb66a4bc98651ecc21a85b8020
denner nemeth <134553090+dennernemethz@users.noreply.github.com>
2024-09-07 15:11:46 -0300

    att

--- ind2.js
+++ ind2.js
@@ -1,23 +1,5 @@
-class nome{
-  static alert=false
-  constructor(primario){
-    this.primario=primario
-  }
-  info=function(){
-    console.log(`nomeP:${this.primario}`)
-    console.log(`ligado ${nome.alert?'sim':'não'}`)
-  
+function ex(max) {
+  return Math.floor(Math.random()* max)
   }
 
-}
-
-let c1 = new nome(100)
-let c2 = new nome(40)
-let c3 = new nome(5)
-
-c1.info()
-c2.info()
-c3.info()
-
-
-nome.alert=true
+  console.log(ex(10))
