27fa30fc6214d4ce84de6f4270662cf35645201b
Denner <dennernemeth@hotmail.com>
2024-10-07 21:47:59 -0300

    add

--- CFB_CURSOS/javascript-lado-cliente/modulos/modulospasta/modulos01.js
+++ CFB_CURSOS/javascript-lado-cliente/modulos/modulospasta/modulos01.js
@@ -1,6 +1,7 @@
 import { nuns } from "./modulos02.js";
 
+import { getAllNumbers } from "./modulos02.js";
+
 console.log(nuns)
+console.log(getAllNumbers())
 
-
-console.log('funfando')
--- CFB_CURSOS/javascript-lado-cliente/modulos/modulospasta/modulos02.js
+++ CFB_CURSOS/javascript-lado-cliente/modulos/modulospasta/modulos02.js
@@ -1,3 +1,9 @@
 let nuns = [1,2,3,4,5,6,7,8,9,10]
 
+
+
+let getAllNumbers=()=>{
+    return nuns
+}
 export {nuns}
+export {getAllNumbers}
