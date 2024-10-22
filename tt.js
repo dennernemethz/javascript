2e53595ac4676ad9d1f547df9c9e509fcb86c771
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2024-10-22 13:54:41 -0300

    att

---
+++ tt.js
@@ -0,0 +1,20 @@
+const ide = Symbol('aqui temos um simbolo')
+
+
+
+console.log(typeof(ide))
+console.log(ide.description)
+
+
+
+let nuns = {
+    nunss:'2',
+    nums:'3'
+}
+
+
+console.log(typeof(nuns))
+
+
+let s_json=JSON.stringify(nuns)
+
