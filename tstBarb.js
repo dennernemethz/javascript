46f50dfdad63a6f69fcaa203862023618538eabc
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-01-08 14:55:54 -0300

    att

--- tstBarb.js
+++ tstBarb.js
@@ -1,9 +1,8 @@
-function geradorDeTabuada(num){
-    for(let i = 1; i <= 10; i ++){
-        console.log(`${num} x ${i} = ${num * i}`);
+function gerationTa(num){
+    for ( let i = 1; i <=10; i ++){
+        console.log(`${num} x ${i} = ${num * i} `)
     }
+    
 }
-
-let numero = 2
-
-geradorDeTabuada(numero)
+let numero = 3
+    gerationTa(numero)
