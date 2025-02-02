c9c52f22bdd04d8ee274d24bbce58924338b9ca8
Denner <dennernemeth@hotmail.com>
2025-02-02 17:09:35 -0300

    att

---
+++ roadMap-from-Developer/loopings/loojs01.js
@@ -0,0 +1,13 @@
+let somando = 1 
+
+let subtraindo = 10 
+
+while(somando <=  10 ){
+    console.log(`somando ${somando}`)
+    somando++
+}
+
+while( subtraindo >= 1){
+    console.log(`subtraindo ${subtraindo}`)
+    subtraindo--
+}
