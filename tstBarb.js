6674924e608b4bb51db41059e006f3db56c55927
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-03-11 15:39:39 -0300

    att

--- tstBarb.js
+++ tstBarb.js
@@ -1,1 +1,17 @@
-function 
+let mercado = {
+
+    nomeProduto:'Manteiga',
+    quantidade:500,
+    compra: function (n){
+        console.log(this)
+        if(n >= this.quantidade){
+            return 'bem maior'
+        }
+        this.quantidade = +n
+    }
+
+}
+
+
+mercado.compra(502)
+console.log(mercado)
