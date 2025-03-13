778591d58f1f75f1334627df2afdf6f7984da5df
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-03-13 15:11:19 -0300

    att

---
+++ tst.js
@@ -0,0 +1,27 @@
+let novo = [{
+    nome:'denner',
+    year:11,
+    occupation:'t.i'
+},{
+    nome:'ana',
+    year:12,
+    occupation:'police'
+},
+{   nome:'junior',
+    year:13,
+    occupation:'fireman'
+},
+
+{
+    nome:'joao',
+    year:14,
+    occupation:'gardner'
+}]
+
+
+
+
+for(let i = 0; i < novo.length; i ++){
+console.log(`o nome do funcionario ${novo[i].nome}, sua idade ${novo[i].year} e sua ocupação ${novo[i].occupation}`)
+
+}
