3c7d5030134fcfb9f04e24b96c66b87065012bbb
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-03-28 14:50:31 -0300

    att

--- jsjs.js
+++ jsjs.js
@@ -1,9 +1,4 @@
-const somar = ()=>{
-    console.log(arguments)
-    let total = 0 
-    for(let i = 0; i < arguments.lenght; i ++){
-        total += arguments[i]
-    }
-    return total
-}
-console.log(somar(1,2,3,4,5,6,7,8,98,4,3,3))
+(function(){
+    let num = 10
+    console.log(num)
+})()
