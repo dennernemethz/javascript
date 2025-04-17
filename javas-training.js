c6916972c0ed5ba73b22b9552f1052f03cd6295b
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-04-17 15:16:44 -0300

    att

--- javas-training.js
+++ javas-training.js
@@ -1,9 +1,11 @@
 let nu = [1,2,3,4,5,6,'ola', false]
 
 
-let arr =  nu.filter( function(i){
+let arr =  nu.some( function(i){
+
   console.log(i)
-  let h = i === 'num'
+  return typeof i === 'number'
+  
 })
 
-console.log(h)
+console.log(arr)
