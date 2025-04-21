7396b97c40d4c79fc3e4fe48981559d8b4424744
Denner <dennernemeth@hotmail.com>
2025-04-20 23:47:24 -0300

    att

--- javas-training.js
+++ javas-training.js
@@ -1,11 +1,8 @@
-let num = [1,2,3,45,6,7,8,9,10,7,3,9,0,7,4]
+let num = [ 1,2,3,4,5,6,7,'ola', true]
 
+const arr1 = num.filter(function (el, i, _arr) {
+  return typeof el === 'number'
+  })
 
-
-let arr  = num.some(function(i){
-  console.log(i)
-  return typeof i === 'Number'
-})
-
-
-console.log(arr)
+  console.log(num)
+  console.log(arr1)
