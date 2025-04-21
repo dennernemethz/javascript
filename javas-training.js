f97480d0d49e56cece629b325e208c722dfdb73f
Denner <dennernemeth@hotmail.com>
2025-04-20 23:07:39 -0300

    att

--- javas-training.js
+++ javas-training.js
@@ -1,10 +1,11 @@
 let num = [1,2,3,45,6,7,8,9,10,7,3,9,0,7,4]
 
 
-let arr = num.every( function(el){
 
-  console.log(el)
-  typeof el === 'number'
+let arr  = num.some(function(i){
+  console.log(i)
+  return typeof i === 'Number'
 })
+
 
 console.log(arr)
