1a11ed4d119facfea3cc9b0af325b8218dce68d5
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-05-02 10:38:55 -0300

    Update javas-training.js

--- javas-training.js
+++ javas-training.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let ar1 = new Array (1,2,3)
 
 let ar2 = Array.from(3)
@@ -6,3 +7,13 @@ let ar3 = Array.of(3)
 console.log(ar1)
 console.log(ar2)
 console.log(ar3)
+=======
+let arr1 = new Array(3)
+console.log(arr1.length)
+
+let arr2 = Array.of(3)
+console.log(arr2.length)
+
+let arr3 = Array.from(1)
+console.log(arr3.length)
+>>>>>>> 07a680667cb1a24eec462a0ef765a10e624cc388
