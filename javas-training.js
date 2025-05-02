3e1f9ef973a5c85b93639826a8cdb8c217dd9a94
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-05-02 16:31:59 -0300

    att

--- javas-training.js
+++ javas-training.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-let ar1 = new Array (1,2,3)
+let arr = new Array(1,2,'oi')
 
-let ar2 = Array.from(3)
-let ar3 = Array.of(3)
-
-console.log(ar1)
-console.log(ar2)
-console.log(ar3)
-=======
-let arr1 = new Array(3)
-console.log(arr1.length)
-
-let arr2 = Array.of(3)
-console.log(arr2.length)
-
-let arr3 = Array.from(1)
-console.log(arr3.length)
->>>>>>> 07a680667cb1a24eec462a0ef765a10e624cc388
+console.log(arr.at)
