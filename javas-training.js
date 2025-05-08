95d5e8e5e07830093a3f97b8e245a6e522940c11
Denner <dennernemeth@hotmail.com>
2025-05-07 23:26:01 -0300

    att

--- javas-training.js
+++ javas-training.js
@@ -1,15 +1,4 @@
-let ar1 = [1,2,3]
+let ar = [1,2,3]
 
-
-let ar2 = [3,5]
-
-function a() {
-    console.log(arguments)
-    console.log(arguments.length)
-  }
-
-
-let ar3 = ar1.push(...ar2)
-
-a(1,3,3)
-a(ar2)
+for(let k in ar){
+    console.log(ar[k])
