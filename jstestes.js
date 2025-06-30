fccf8c1090025972bd551aba1b4acb30cf069f8c
Denner <dennernemeth@hotmail.com>
2025-06-29 21:49:23 -0300

    att

---
+++ jstestes.js
@@ -0,0 +1,27 @@
+let objetos1 = {
+    nome:'denner'
+}
+
+let objetos2 =  new Object("denner")
+
+let numeros = new Number(10)
+
+let string = new String("denner")
+
+
+
+
+
+console.log(objetos1)
+console.log(typeof(objetos1))
+
+console.log(objetos2)
+console.log(typeof(objetos2))
+console.log(objetos2.length)
+console.log(objetos2.valueOf())
+
+console.log(numeros)
+console.log(typeof(numeros))
+
+console.log(string)
+console.log(typeof(string))
