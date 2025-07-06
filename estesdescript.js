726bcebe63728537d749dfddf26b89cab0538e3d
Denner <dennernemeth@hotmail.com>
2025-07-06 20:20:03 -0300

    att

--- estesdescript.js
+++ estesdescript.js
@@ -1,9 +1,14 @@
+function ob(bj01) { 
+    console.log(bj01)
+ }
+
+
 let obj1 = {
     nome:"denner",
-    idade:10
+    idade:10,
+    ob:ob
 }
 
-let obj2 = new Object("minhs string")
 
-console.log(obj2.lenght)
-console.log(obj2.valueOf())
+
+console.log(obj1)
