954d068cf563c7a7172de9499e26f5bff7de7746
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-07-08 15:33:48 -0300

    att

--- estesdescript.js
+++ estesdescript.js
@@ -1,9 +1,26 @@
-let obj1 = {
-    nome:"denner",
-    idade:10
+function change(name){
+this.name = name
+this.timeSquare = new Date()
+
 }
 
-let obj2 = new Object("minhs string")
 
-console.log(obj2.lenght)
-console.log(obj2.valueOf())
+let obj1 = {
+    name:'denner',
+    numero:10,
+    timeSquare:11,
+    change
+}
+
+let obj2 = {
+    name:'denner',
+    numero:10,
+    timeSquare:11,
+    change
+}
+
+obj1.change.name = 'modifiquei'
+
+console.log(obj1)
+
+
