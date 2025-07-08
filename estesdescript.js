14e3a819278d7e644784a5d26b82362d8c0e2d41
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-07-08 16:29:27 -0300

    att

--- estesdescript.js
+++ estesdescript.js
@@ -1,50 +1,9 @@
-<<<<<<< HEAD
-function change(name){
-this.name = name
-this.timeSquare = new Date()
-
+let população ={
+    femea:10,
+    macho:12
 }
 
 
-let obj1 = {
-    name:'denner',
-    numero:10,
-    timeSquare:11,
-    change
-}
+let generos = Object.keys(população)
 
-let obj2 = {
-    name:'denner',
-    numero:10,
-    timeSquare:11,
-    change
-}
-
-obj1.change.name = 'modifiquei'
-
-console.log(obj1)
-
-
-=======
-function cell(name){
-   this.name = name
-   this.time = new Date()
-}
-
-const obj1 = {
-    name:'denner',
-    time:'hrs',
-    cell
-}
-
-const obj2 = {
-    name:'denner2',
-    time:'hrs2',
-    cell
-}
-
-
-
-obj1.cell("mofiquei")
-console.log(obj1)
->>>>>>> 91cee9b08b9d2f6edd8b35f8c3340f2dfb54cfe1
+console.log(generos[0])
