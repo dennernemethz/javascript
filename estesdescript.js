793b5d846b0ca7d741863a4a824a450d8e75304b
Denner <dennernemeth@hotmail.com>
2025-07-11 00:21:08 -0300

    att

--- estesdescript.js
+++ estesdescript.js
@@ -1,13 +1,13 @@
-function  Task (name){
-    this.name = name
-    this.time = new Date()
-    this.changeName = function (name1) {
-        this.name = name1
-      }
+function FunçãoConstrutora (name){
+      this.name = name
+      this.outraFunction= function (outroNome) {
+        this.name = outroNome
+        }
 }
 
+let obj = new FunçãoConstrutora('denner')
 
-let task1 = new Task('aqui é o parametro')
-task1.changeName = 'modifiqueio change name '
+obj.outraFunction = 'new'
 
-console.log(task1
+
+console.log(obj)
