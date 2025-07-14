99f963cd2452a96d4d57582498cbf02b81ce5e1f
Denner <dennernemeth@hotmail.com>
2025-07-14 16:10:01 -0300

    ç

--- jstestes.js
+++ jstestes.js
@@ -1,11 +1,19 @@
-let obj = {
-    nome:'denner',
-    numero:10,
+function task (name){
+    let _name = name
 
-    sioh(){
-        console.log(obj.nome)
+    this.creatAt = new Date()
+    this.updateAt = null
+    this.changeName = function(newName){
+        _name = newName  // como o name "perdeu" valor quando usamos a variavel, aqui chamamos o _name que vai ser modificado no mesmo instante que mudarmos e chamarmos o newName
     }
+    this.getName = function () {
+        return _name // aqui ao chamarmos o task no console e chamarmos a function getName dentro dele, recuperamos o valor name que na qual foi perdido, isso tem que ser dentro do escopo da function TASK. 
+      }
 }
 
+const task1 =  new task ('minha primeira tarefa')
+const task2 = new task('minha segunda tarefa')
 
-obj.sioh()
+console.log(task1.getName())
+
+task1.changeName('minha tarefa atualizada')
