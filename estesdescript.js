b98eb97d7756d66195011e6637868b1417137af6
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-07-30 14:19:10 -0300

    att

--- estesdescript.js
+++ estesdescript.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-=======
-let p1 = 'olá mundo'
 
-//console.log(p1.replace('oi,oi','olá'))
+let k = {}
 
 
-let p2 = 'whta'
+k['f']='denner'
 
-console.log(p2.replace(p1))
->>>>>>> 9cc8cb279f56b3d68e471adf5f903f2a874948cc
+
+console.log(k)
