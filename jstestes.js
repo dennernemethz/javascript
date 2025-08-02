256813e703b50530643dc44851a09878c92e417d
Denner <dennernemeth@hotmail.com>
2025-08-02 00:55:55 -0300

    a

--- jstestes.js
+++ jstestes.js
@@ -1,3 +1,6 @@
-let ar = [12]
+let n1 = "minha primeira string"
 
-console.log()
+console.log(n1.replace("i","u")) // vai modificar o i pelo u
+console.log(n1)// replace(/i/g, "u") vai fazer com que todos os i sejam substituidos
+
+console.log(n1.indexOf('i'))// vai mostrar o local da primeira letra i "1"
