394170d7a99d557c91018e90aa568c5b814cf89d
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-08-13 15:18:35 -0300

    att

--- jstestes.js
+++ jstestes.js
@@ -1,15 +1,9 @@
-let n = ' minha string '
+let n = '     minha string       2 '
 
-let n2 = new String('minha segunda string')
+let n2 = "            " + n
 
-
-console.log(n.valueOf())
-console.log(n.toString())
-
-console.log(n2.valueOf())
-console.log(n2.toString())
-
-console.log(typeof(n))
-console.log(typeof(n2))
-console.log(n)
+console.log(n2)
+console.log(n2.trim())
+console.log(n2.trimEnd())
+console.log(n2.trimStart())
 console.log(n2)
