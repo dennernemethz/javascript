ebb24ab71073b23b7357d85aef529ab23e95c629
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-08-16 16:24:29 -0300

    att

--- jstestes.js
+++
@@ -1,9 +0,0 @@
-let n = '     minha string       2 '
-
-let n2 = "            " + n
-
-console.log(n2)
-console.log(n2.trim())
-console.log(n2.trimEnd())
-console.log(n2.trimStart())
-console.log(n2)
