3f21db9702f7c087de619f492f206c2003e3cf63
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-08-22 14:49:12 -0300

    att

--- estesdescript.js
+++ estesdescript.js
@@ -1,13 +1,10 @@
-let num = 10
-let num2 = 122
+function dividir(x, y){
+    if(x / y < Number.MIN_VALUE){
+        return 'progresso'
+    }
+    return x/y
+}
 
-console.log(num.toString(3))
-console.log((20) .toString(16))
+console.log(dividir(4,1))
 
-
-console.log(num2.toFixed(6))
-
-
-
-console.log(num2.toPrecision(9)) 
-
+console.log(dividir(7, 1))
