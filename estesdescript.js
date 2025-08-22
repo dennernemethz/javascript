6546174c3645199618c41692b09a038eb6258b7f
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-08-22 12:04:59 -0300

    ATT

--- estesdescript.js
+++ estesdescript.js
@@ -1,8 +1,13 @@
 let num = 10
+let num2 = 122
 
 console.log(num.toString(3))
 console.log((20) .toString(16))
 
-console.log(num.toFixed(5))
 
-console.log(num.toPrecision(7))
+console.log(num2.toFixed(6))
+
+
+
+console.log(num2.toPrecision(9)) 
+
