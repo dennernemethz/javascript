6cff9bcc9e037bb48f3c1a027cbd83128082c9b3
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-08-28 16:23:40 -0300

    att

--- estesdescript.js
+++ estesdescript.js
@@ -1,4 +1,1 @@
-let num = [1,2,3,4,5,6,7,8,9,10] 
-
-
-console.log(Math.random(...num)* 10)
+console.log(Math.random() * 10 )
--- jstestes.js
+++
@@ -1,7 +0,0 @@
-let num = [1,2,3,4,5,6,7,8,9]
-
-console.log(Math.floor())  // esse metodo vai arredodar sempre para baixo 
-console.log(Math.round())// a partir de um numero quebrado ex 45.5  a partir do 5 arredonda para 46 45.4 vai ficar 45 mesmo
-console.log(Math.ceil()) // esse metodo arrendoda de qlq forma
-
-console.log(Math.pow(2, 3)) // operador de potencia 
