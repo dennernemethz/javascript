6e1ed8732783e41e60f42057b7cf6106331dbcd4
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-09-03 14:49:49 -0300

    att

--- estesdescript.js
+++ estesdescript.js
@@ -1,3 +1,9 @@
-for ( let i = 1; i<=10; i ++){
-    console.log("numeros:  " + i)
-}
+let data =  new Date()
+
+console.log(data.toISOString())
+console.log(data.toUTCString())
+console.log(data.toDateString())
+console.log(data.toLocaleDateString())
+console.log(data.toString())
+console.log(data.toLocaleTimeString())
+console.log(data.toLocaleString()) // posso passar um parametro como  "en-USA", vai mostrar o dia e hora local do pais 
