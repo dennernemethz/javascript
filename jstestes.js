8adb88a779f570d5138a6445bcc8802f7cb969bc
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-09-30 12:53:56 -0300

    att

--- jstestes.js
+++ jstestes.js
@@ -1,37 +1,7 @@
-/*
-// desafio criar um função e receber um string e na saida do console ela sair invertida
+let num = [1,2,3,4,5,6,7,8,9,10]
 
-function inverter (str){ // recebendo um parametro qualquer string para poder retornar ela no console
+let [primeiro,,terceiro,,,,,,nono]= num
 
-
-return str.split('').reverse().join('');  // O método split() divide uma String em uma lista ordenada de substrings, coloca essas substrings em um array e retorna o array
-// reverse() vai mostrar  astring reversa
-// O método join() junta todos os elementos de um array (ou um array-like object) em uma string e retorna esta string.
-
- 
-}
-
-console.log(inverter('denner')) // renned  
-
-
-// mais explicado 
-
-
-function inverter(str) {
-  // Passo 1: Divide a string em um array de caracteres
-  const arrayDeLetras = str.split('');
-  // Exemplo: "banana" => ['b', 'a', 'n', 'a', 'n', 'a']
-
-  // Passo 2: Inverte a ordem dos elementos do array
-  const arrayInvertido = arrayDeLetras.reverse();
-  // Resultado: ['a', 'n', 'a', 'n', 'a', 'b']
-
-  // Passo 3: Junta o array de volta em uma string
-  const stringInvertida = arrayInvertido.join('');
-  // Resultado: "ananab"
-
-  // Retorna a string invertida
-  return stringInvertida;
-}
-*/
-
+console.log(primeiro)
+console.log(terceiro)
+console.log(nono)
