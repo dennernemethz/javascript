eaa16b54c52398ccd183daa71791ac53d4f39d96
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-10-03 13:11:55 -0300

    a

---
+++ roadMap-from-Developer/funçoes/variaveis daclarada/varDeclarada02.js
@@ -0,0 +1,8 @@
+let msg = ' hello everyone'
+
+function showMessage(){
+    let msg1 = msg + ' im denner' // podemos acessar outras variaveis dentro da função 
+    console.log(msg1)
+}
+
+showMessage()
