01b62e4bc760e8fc065a5e689e4ae344933cb1e1
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-10-10 13:47:35 -0300

    att

--- tst1.js
+++
@@ -1,6 +0,0 @@
-let ar  = ['azul','amarelo','roxo','vermelho']
-
-let algunsArs = ar.slice(1,2)
-
-console.log(algunsArs) 
- //método slice() em arrays (em JavaScript, por exemplo) é utilizado para criar uma cópia rasa de uma parte de um array, sem modificar o array original.
