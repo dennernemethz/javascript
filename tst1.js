39b2f89694b9caf55e8e256554c96f3a213a57af
Denner <dennernemeth@hotmail.com>
2025-11-04 17:39:06 -0300

    att

--- tst1.js
+++ tst1.js
@@ -1,18 +1,15 @@
 let obj = {
-    value:0,
-    soma(n){
-     this.value += n
-     return this
-    },
-    subtrai(n){
-     this.value -= n
-     return this
-    },
-    log(){
- console.log(this.value)
- return this
-    }
+ key:0,
+ soma(n){
+    this.key += n
+ },
+ subtração(n){
+    this.key -= n
+ },
+ log(){
+    console.log(this.key)
+    return this 
+ }
 }
 
-obj.soma(3).subtrai(1).soma(4)
-console.log(obj.value)
+obj.soma(3).subtração(4).log()
