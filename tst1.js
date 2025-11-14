36ccf0c9913fd851e5687eb73807d5692f3a4c06
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-11-14 13:49:16 -0300

    att

--- tst1.js
+++ tst1.js
@@ -1,5 +1,11 @@
-class nomes {
-    constructor() {
-        
+function Animal(nome, idade){
+    this.nome = nome
+    this.idade = idade
+    ola(){
+        console.log(this.nome, this.idade)
     }
 }
+
+let p1 = new Animal('denner', 10)
+
+p1.ola()
