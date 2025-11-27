65257dd7ca10bc8a7c3512bacbe0ec5d676a7cef
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-11-27 14:15:52 -0300

    att

--- tst1.js
+++ tst1.js
@@ -1,19 +1,19 @@
 class Pai {
-    constructor(){
-        this.dizendoOla = 'eu sou pai'
+    constructor() {
+        if(new.target === Pai)
+        throw Error ('Essa classe é abstrata')
     }
     ola(){
-        console.log(this.dizendoOla)
+        throw Error ('deve ser invocada a cada uma das instancias')
+    
     }
 }
 
-
 class Filho extends Pai{
-    constructor(){
-        super()
+    ola(){
+        console.log('sendo invocado da classe filho')
     }
 }
 
 let f1 = new Filho()
-
 f1.ola()
