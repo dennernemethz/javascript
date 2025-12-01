97b5d8d3b2c491b1987e2f8facaeca810866c47a
Denner <dennernemeth@hotmail.com>
2025-12-01 12:57:27 -0300

    att

--- tst1.js
+++ tst1.js
@@ -1,7 +1,19 @@
-class Soma { 
-    static  somar(a,b){
-        console.log(a + b)
+class Pai {
+    constructor (){
+        this.mensagem = 'oi eu sou o pai'
+    }
+    static soma(a,b){
+        console.log(a+b)
     }
 }
 
-Soma.somar(11,2)
+class Filho extends Pai {
+     constructor(){
+        super()
+     }
+}
+
+let f1 = new Filho()
+console.log(f1.mensagem)
+
+Pai.soma(2,7)
