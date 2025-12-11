7f08487c28061bb09252debaeb9cb6f0220ecb40
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-12-11 13:11:43 -0300

    att

--- tst1.js
+++ tst1.js
@@ -1,22 +1,5 @@
-<<<<<<< HEAD
-=======
-class Pai {
-    constructor (){
-        this.mensagem = 'oi eu sou o pai'
-    }
-    static soma(a,b){
-        console.log(a+b)
-    }
-}
+let nomes = ['java','css']
 
-class Filho extends Pai {
-     constructor(){
-        super()
-     }
-}
-
-let f1 = new Filho()
-console.log(f1.mensagem)
-
-Pai.soma(2,7)
->>>>>>> 97b5d8d3b2c491b1987e2f8facaeca810866c47a
+nomes.map((p)=>{
+    console.log(p)
+})
