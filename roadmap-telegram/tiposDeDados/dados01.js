17e0a40baf3a76425535cb9e353131bcf5d7e9d3
Denner Nemeth de Queiroz <dennernemeth@hotmail.com>
2025-12-18 14:41:00 -0300

    att

---
+++ roadmap-telegram/tiposDeDados/dados01.js
@@ -0,0 +1,15 @@
+let numberr = 2
+let string  = 'ola mundo'
+let boleano1 = true
+let boleano2 = false
+let indefinido 
+console.log(indefinido)
+
+let aRRAY = [1,2,3,4,5]
+
+let obj = {
+    nome:'denner',
+    sobrenome:'nemeth'
+}
+
+// esse são dados tipos de dados no javascript
