f44f97e0060fe76d7c184cddebb746cbe6fe517b
Denner <dennernemeth@hotmail.com>
2026-01-10 17:36:44 -0300

    att

--- jstestes.js
+++ jstestes.js
@@ -1,3 +1,17 @@
-let carro ={
-    
+let meuCarro ={
+    carroLigado:'ligado',
+    ligar(){
+        console.log('meu carro está ' + this.carroLigado)
+    },
+    carroDesligado:'desligado',
+    desligar(){
+        console.log('meu carro está ' + this.carroLigado)
+    },
+    acelerando:'acelerando',
+    acelerar(){
+        console.log(this.acelerando)
+    }
+
 }
+
+meuCarro.acelerar()
